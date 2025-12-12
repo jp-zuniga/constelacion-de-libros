702c13901db5ba8d4471d69aaa4376bbcef60dbf
cowpybara <azarodriguez@uamv.edu.ni>
2025-12-12 15:27:13 -0600

    ola kevin solo falta lo tuyo

---
+++ src/main/java/org/ConstelacionDeLibros/models/pagos/Deuda.java
@@ -0,0 +1,63 @@
+package org.ConstelacionDeLibros.models.pagos;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.ConstelacionDeLibros.config.Auditable;
+import org.ConstelacionDeLibros.config.Auditoria;
+import org.ConstelacionDeLibros.config.AuditoriaListener;
+import org.ConstelacionDeLibros.models.BaseEntity;
+import org.ConstelacionDeLibros.models.usuarios.Cliente;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@EntityListeners(AuditoriaListener.class)
+@Getter @Setter
+public
+class Deuda extends BaseEntity implements Auditable {
+
+    @Embedded
+    private Auditoria auditoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cliente", nullable = false)
+    @Required
+    private Cliente cliente;
+
+    @Column(nullable = false, scale = 2)
+    @Required
+    private BigDecimal montoAdeudado;
+
+    @Column(nullable = false)
+    @Required
+    private LocalDate fechaEmision;
+
+    @Column(nullable = false)
+    private boolean cancelada = false;
+
+    @Override
+    public
+    Auditoria getAuditoria() {
+        return auditoria;
+    }
+
+    @Override
+    public
+    void setAuditoria(Auditoria auditoria) {
+        this.auditoria = auditoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Deuda{" +
+                "auditoria=" + auditoria +
+                ", cliente=" + cliente +
+                ", montoAdeudado=" + montoAdeudado +
+                ", fechaEmision=" + fechaEmision +
+                ", cancelada=" + cancelada +
+                '}';
+    }
+}
---
+++ src/main/java/org/ConstelacionDeLibros/models/pagos/Multa.java
@@ -0,0 +1,62 @@
+package org.ConstelacionDeLibros.models.pagos;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.ConstelacionDeLibros.config.Auditable;
+import org.ConstelacionDeLibros.config.Auditoria;
+import org.ConstelacionDeLibros.config.AuditoriaListener;
+import org.ConstelacionDeLibros.models.BaseEntity;
+import org.ConstelacionDeLibros.models.operaciones.Prestamo;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditoriaListener.class)
+@Getter @Setter
+public
+class Multa extends BaseEntity implements Auditable {
+
+    @Embedded
+    private Auditoria auditoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_prestamo", nullable = false)
+    @Required
+    private Prestamo prestamo;
+
+    @Column(length = 80, nullable = false)
+    @Required
+    private String nombre;
+
+    @Column(length = 200)
+    private String descripcion;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Required
+    private EstadoMulta estado;
+
+    @Override
+    public
+    Auditoria getAuditoria() {
+        return auditoria;
+    }
+
+    @Override
+    public
+    void setAuditoria(Auditoria auditoria) {
+        this.auditoria = auditoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Multa{" +
+                "auditoria=" + auditoria +
+                ", prestamo=" + prestamo +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+}
